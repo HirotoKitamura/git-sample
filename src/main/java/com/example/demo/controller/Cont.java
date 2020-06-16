66c6357438f2c065c87ca18c5d19af05e6f840df
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 12:22:32 +0900

    nemui

---
+++ src/main/java/com/example/demo/controller/Cont.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("git")
+public class Cont {
+	@RequestMapping("")
+	public String index() {
+		return "おはようさん";
+	}
+}
